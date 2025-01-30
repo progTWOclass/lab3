a24dfc02f117778e93e572966771b043c8c2ed7e
Steve Banh <steve.banh02@gmail.com>
2025-01-29 19:49:13 -0500

    q1 done

---
+++ src/Question1.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Question1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> toDoList = new ArrayList<>();
+
+        System.out.println("enter a to do task in your list");
+
+        while(true){
+            String toDo = input.nextLine();
+            toDoList.add(toDo);
+
+            System.out.println("do you want to add more task? ");
+            String answer = input.nextLine();
+            if(answer.equalsIgnoreCase("no"))
+                break;
+
+            System.out.println("enter your next tasks: ");
+        }
+        System.out.println("your list has: ");
+        System.out.println(toDoList);
+    }
+}
